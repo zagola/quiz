561793069815c8bab9c5675101934184eb0fe28d
zagola <zagola@github.com>
2024-02-26 12:19:57 +0100

    Question.class with methods that retrieve a question and return an answer

---
+++ src/main/java/org/example/Question.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Question {
+    private String question;
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private  String correctAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+}
